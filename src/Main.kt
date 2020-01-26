25768e3d539d4d7e1e4e21d132faaf5addad8b10
mrDevGo <mrdevgo@gmail.com>
2020-01-26 10:57:36 +0300

    add Enums

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,11 @@
+
 import java.util.*
 
 var inValue: Double = 0.0
 lateinit var fromType: String
 lateinit var toType: String
-lateinit var currentFromType: Type
-lateinit var currentToType: Type
-
-
-
-data class Type(val courtName: String,
-                val singularName: String,
-                val pluralName: String,
-                val K: Double) {
-
-    fun findItems(value: String): Boolean {
-        return value == courtName || value == singularName || value == pluralName
-    }
-}
+lateinit var currentFromUnit: Units
+lateinit var currentToUnit: Units
 
 fun main() {
     val scanner = Scanner(System.`in`)
@@ -29,46 +18,17 @@ fun main() {
             fromType = input[1].toLowerCase()
             toType = input[3].toLowerCase()
         }
-        toConvert()
+        currentFromUnit = Units.init(fromType)!!
+        currentToUnit = Units.init(toType)!!
+        printResult()
     }
-}
-
-fun toConvert() {
-    addItemsInList().forEach {
-        if (it.findItems(fromType)) {
-            currentFromType = it
-        }
-        if (it.findItems(toType)) {
-            currentToType = it
-        }
-    }
-    printResult()
 }
 
 private fun printResult() {
-    val result = inValue * currentFromType.K / currentToType.K
-    val resultFromType = if (inValue != 1.0) currentFromType.pluralName else currentFromType.singularName
-    val resultToType = if (result != 1.0) currentToType.pluralName else currentToType.singularName
+    val result = inValue * currentFromUnit.K / currentToUnit.K
+    val resultFromType = if (inValue != 1.0) currentFromUnit.pluralName else currentFromUnit.singularName
+    val resultToType = if (result != 1.0) currentToUnit.pluralName else currentToUnit.singularName
     println("$inValue $resultFromType is $result $resultToType")
 }
-
-private fun addItemsInList(): List<Type> {
-    val listItems = mutableListOf<Type>()
-    listItems.add(Type("m", "meter", "meters", 1.0))
-    listItems.add(Type("km", "kilometer", "kilometers", 1000.0))
-    listItems.add(Type("cm", "centimeter", "centimeters", 0.01))
-    listItems.add(Type("mm", "millimeter", "millimeters", 0.001))
-    listItems.add(Type("mi", "mile", "miles", 1609.35))
-    listItems.add(Type("yd", "yard", "yards", 0.9144))
-    listItems.add(Type("ft", "foot", "feet", 0.3048))
-    listItems.add(Type("in", "inch", "inches", 0.0254))
-    listItems.add(Type("g", "gram", "grams", 1.0))
-    listItems.add(Type("kg", "kilogram", "kilograms", 1000.0))
-    listItems.add(Type("mg", "milligram", "milligrams", 0.001))
-    listItems.add(Type("lb", "pound", "pounds", 453.592))
-    listItems.add(Type("oz", "ounce", "ounces", 28.3495))
-    return listItems
-}
-
 
 
