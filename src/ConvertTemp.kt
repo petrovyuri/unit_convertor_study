2a5af4644f3d86ad0c4008741479b3c6c6b906a3
mrDevGo <mrdevgo@gmail.com>
2020-01-26 19:28:35 +0300

    Is complete

--- src/ConvertTemp.kt
+++ src/ConvertTemp.kt
@@ -15,8 +15,12 @@ class ConvertTemp {
             val result = inValue + 273.15
             val (resultFromType, resultToType) = pair(result)
             println("$inValue $resultFromType is $result $resultToType")
-        } else {
+        } else if (currentFromUnit.name == "FAHRENHEIT") {
             val result = (inValue + 459.67) * 5 / 9
+            val (resultFromType, resultToType) = pair(result)
+            println("$inValue $resultFromType is $result $resultToType")
+        } else {
+            val result = inValue
             val (resultFromType, resultToType) = pair(result)
             println("$inValue $resultFromType is $result $resultToType")
         }
@@ -27,8 +31,12 @@ class ConvertTemp {
             val result = inValue * 9 / 5 + 32
             val (resultFromType, resultToType) = pair(result)
             println("$inValue $resultFromType is $result $resultToType")
-        } else {
+        } else if(currentFromUnit.name == "KELVIN") {
             val result = (inValue * 9 / 5) - 459.67
+            val (resultFromType, resultToType) = pair(result)
+            println("$inValue $resultFromType is $result $resultToType")
+        }else{
+            val result = inValue
             val (resultFromType, resultToType) = pair(result)
             println("$inValue $resultFromType is $result $resultToType")
         }
@@ -39,8 +47,12 @@ class ConvertTemp {
             val result = inValue - 273.15
             val (resultFromType, resultToType) = pair(result)
             println("$inValue $resultFromType is $result $resultToType")
+        } else if (currentFromUnit.name == "FAHRENHEIT") {
+            val result = (inValue - 32) * 5 / 9
+            val (resultFromType, resultToType) = pair(result)
+            println("$inValue $resultFromType is $result $resultToType")
         } else {
-            val result = (inValue - 32) *5 / 9
+            val result = inValue
             val (resultFromType, resultToType) = pair(result)
             println("$inValue $resultFromType is $result $resultToType")
         }
